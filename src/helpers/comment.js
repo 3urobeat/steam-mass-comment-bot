7a62f0845b13d4d4c22528e0de21024410c45172
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-01-01 18:13:03 +0100

    Update every file header to ISO date & update year in copyright notice

--- src/helpers/comment.js
+++ src/helpers/comment.js
@@ -1,13 +1,13 @@
 /*
  * File: commentProfile.js
  * Project: steam-mass-comment-bot
- * Created Date: 23.01.2022 16:32:05
+ * Created Date: 2022-01-23 16:32:05
  * Author: 3urobeat
  *
- * Last Modified: 15.08.2023 17:32:27
+ * Last Modified: 2024-01-01 18:07:41
  * Modified By: 3urobeat
  *
- * Copyright (c) 2022 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2022 - 2024 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
--- src/sessions/helpers/tokenStorageHandler.js
+++ src/sessions/helpers/tokenStorageHandler.js
@@ -1,13 +1,13 @@
 /*
  * File: tokenStorageHandler.js
- * Project: steam-idler
- * Created Date: 10.10.2022 12:53:20
+ * Project: steam-mass-comment-bot
+ * Created Date: 2022-10-10 12:53:20
  * Author: 3urobeat
  *
- * Last Modified: 15.08.2023 17:03:40
+ * Last Modified: 2024-01-01 18:09:25
  * Modified By: 3urobeat
  *
- * Copyright (c) 2022 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2022 - 2024 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
