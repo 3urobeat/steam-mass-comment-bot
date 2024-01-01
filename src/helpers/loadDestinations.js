a994eed6f4331e148caf9ee375264081ed7cefef
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-01-01 20:44:11 +0100

    Port over steam-comment-service-bot's handleSteamIdResolving()

--- src/helpers/loadDestinations.js
+++ src/helpers/loadDestinations.js
@@ -4,7 +4,7 @@
  * Created Date: 2022-01-23 15:28:34
  * Author: 3urobeat
  *
- * Last Modified: 2024-01-01 18:07:31
+ * Last Modified: 2024-01-01 20:42:59
  * Modified By: 3urobeat
  *
  * Copyright (c) 2022 - 2024 3urobeat <https://github.com/3urobeat>
@@ -16,119 +16,172 @@
 
 
 const config          = require("../../config.json");
-const steamidResolver = require("steamid-resolver");
-const fs              = require("fs");
+const SteamID         = require("steamid");
+const steamIDResolver = require("steamid-resolver");
+
+
+// This is a slimmed down version of this function from my steam-comment-service-bot: https://github.com/3urobeat/steam-comment-service-bot/blob/master/src/controller/helpers/handleSteamIdResolving.js
+/**
+ * Handles converting URLs to steamIDs and determining their type
+ * Note: For discussions only type checking/determination is supported and you need to provide a full URL.
+ * @param {string} str The profileID argument provided by the user
+ * @param {function(string|null, string|null, string|null): void} callback Called with `err` (String or null), `steamID64` (String or null), `idType` (String or null) parameters on completion
+ */
+function handleSteamIdResolving(str, callback) {
+
+    // Instantly callback nothing if nothing was provided
+    if (!str) return callback(null, null);
+
+    // "profile", "group" or "sharedfile" - Is populated before making callback
+    let idType;
+
+    // Function to handle steamIDResolver callbacks as they are always roughly the same. Only call for profile & group!
+    function handleResponse(err, res) { //eslint-disable-line
+        logger("debug", `handleSteamIdResolving: handleResponse(): Received callback from steamid-resolver. err: ${err} | res: ${res}`);
+
+        // Check if resolving failed
+        if (err) return callback(err, null, null);
+
+        // Quickly determine type. We know that the ID here must be valid and of type profile or group as sharedfile is recognized as invalid by SteamID
+        idType = new SteamID(res).type == SteamID.Type.INDIVIDUAL ? "profile" : "group";
+
+        callback(null, res, idType);
+    }
+
+    // Try to figure out if user provided an steamID64 or a customURL or a whole profile link
+    if (isNaN(str) || !new SteamID(str).isValid()) { // If not a number or invalid SteamID. Note: Sharedfile IDs are considered invalid.
+        if (str.includes("steamcommunity.com/id/")) {
+            logger("debug", "handleSteamIdResolving: User provided customURL profile link...");
+
+            steamIDResolver.customUrlToSteamID64(str, handleResponse);
+
+        } else if (str.includes("steamcommunity.com/profiles/")) {
+            logger("debug", "handleSteamIdResolving: User provided steamID64 profile link...");
+
+            // My library doesn't have a check if exists function nor returns the steamID64 if I pass it into steamID64ToCustomUrl(). But since I don't want to parse the URL myself here I'm just gonna request the full obj and cut the id out of it
+            steamIDResolver.steamID64ToFullInfo(str, (err, obj) => handleResponse(err, obj.steamID64[0]));
+
+        } else if (str.includes("steamcommunity.com/discussions/forum") || /steamcommunity.com\/app\/.+\/discussions/g.test(str) || /steamcommunity.com\/groups\/.+\/discussions/g.test(str)) {
+            logger("debug", "handleSteamIdResolving: User provided discussion link...");
+
+            idType = "discussion";
+
+            callback(null, str, idType);
+
+        } else if (str.includes("steamcommunity.com/groups/")) { // Must be below the discussion check because of the "groups/abc/discussion" regex above
+            logger("debug", "handleSteamIdResolving: User provided group link...");
+
+            steamIDResolver.groupUrlToGroupID64(str, handleResponse);
+
+        } else if (str.includes("steamcommunity.com/sharedfiles/filedetails/?id=")) {
+            logger("debug", "handleSteamIdResolving: User provided sharedfile link...");
+
+            // Check if ID is valid
+            steamIDResolver.isValidSharedfileID(str, (err, res) => {
+                if (err) return callback(err, null, null);
+                if (!res) return callback("The specified sharedfile could not be found", null, null);
+
+                // Cut domain away
+                let split = str.split("/");
+                if (split[split.length - 1] == "") split.pop(); // Remove trailing slash (which is now a space because of split("/"))
+
+                str = split[split.length - 1].replace("?id=", "");
+
+                // Update idType
+                idType = "sharedfile";
+
+                callback(null, str, idType);
+            });
+
+        } else { // Doesn't seem to be an URL. We can ignore discussions as we need to provide an URL to SteamCommunity.
+
+            logger("debug", "handleSteamIdResolving: Trying to figure out what has been provided...");
+
+            steamIDResolver.customUrlToSteamID64(str, (err, steamID64) => { // Check profile first, as it will probably be used more often
+                if (err) {
+                    logger("debug", "handleSteamIdResolving: profile id check returned an error. Trying group id check...");
+
+                    steamIDResolver.groupUrlToGroupID64(str, (err, groupID) => {
+                        if (err) {
+                            logger("debug", "handleSteamIdResolving: group id check returned an error. Trying sharedfile id check...");
+
+                            steamIDResolver.isValidSharedfileID(str, (err, isValid) => {
+                                if (err || !isValid) {
+                                    logger("debug", "handleSteamIdResolving: sharedfile id check also returned an error! Resolving with error as something unknown was provided: " + str);
+                                    handleResponse("ID parameter seems to be invalid.", null);
+
+                                } else {
+
+                                    logger("debug", "handleSteamIdResolving: the provided id seems to be a sharedfile id! Returning sharedfileID...");
+
+                                    if (str.includes("steamcommunity.com/")) { // Check if full URL was provided and cut domain away
+                                        let split = str.split("/");
+                                        if (split[split.length - 1] == "") split.pop(); // Remove trailing slash (which is now a space because of split("/"))
+
+                                        str = split[split.length - 1].replace("?id=", "");
+                                    }
+
+                                    // Update idType
+                                    idType = "sharedfile";
+
+                                    callback(null, str, idType);
+                                }
+                            });
+
+                        } else {
+                            logger("debug", "handleSteamIdResolving: the provided id seems to be a group id! Returning groupID...");
+                            handleResponse(null, groupID);
+                        }
+                    });
+
+                } else {
+                    logger("debug", "handleSteamIdResolving: the provided id seems to be a profile id! Returning steamID64...");
+                    handleResponse(null, steamID64);
+                }
+            });
+
+        }
+
+    } else {
+
+        logger("debug", "handleSteamIdResolving: I don't need to convert anything as user seems to have already provided an steamID64. Cool!");
+
+        // Quickly determine type. We know that the ID here must be valid and of type profile or group as sharedfile is recognized as invalid by SteamID
+        idType = new SteamID(str).type == SteamID.Type.INDIVIDUAL ? "profile" : "group";
+
+        handleResponse(null, str, idType);
+    }
+
+}
 
 
 /**
- * Loads profiles from config and converts them, if necessary, to steamID64
- * @returns {Promise.<string[]>} Returns a promise that resolves with an array of strings
+ * Loads all destinations from the config, determines type and converts them to steamID64
  */
-module.exports.loadProfiles = () => {
+module.exports.loadDestinations = function() {
     return new Promise((resolve) => {
 
-        let profiles = [];
-        let resolverCalls = 0;
-        let skipped = [];
-        let emptyStr = 0;
+        let results = [];
 
-        config.profiles.forEach((e) => {
-            if (String(e).includes("steamcommunity.com/profiles") || String(e).includes("steamcommunity.com/id")) {
-                resolverCalls++;
+        config.destinations.forEach((e, i) => {
+            setTimeout(() => {
 
-                setTimeout(() => {
-                    steamidResolver.customUrlTosteamID64(e, (err, steamID64) => {
-                        if (err) {
-                            logger("err", `I couldn't get the ID of ${e}. It will be excluded. Error: ${err}`);
-                            skipped.push(e);
-                        }
+                handleSteamIdResolving(e, (err, id, type) => {
+                    if (err) {
+                        logger("error", `Failed to resolve destination '${e}', ignoring it. ${err}`);
+                    } else {
+                        results.push({
+                            raw: e,
+                            processed: id,
+                            type: type
+                        });
+                    }
 
-                        profiles.push(steamID64);
-                    });
-                }, 500 * resolverCalls);
-
-            } else {
-
-                if (e != "") profiles.push(e);
-                    else emptyStr++;
-            }
-        });
-
-
-        let profileInterval = setInterval(() => {
-
-            if (profiles.length + skipped.length + emptyStr == config.profiles.length) {
-                clearInterval(profileInterval);
-
-                // Write result to make it
-                config.profiles = profiles;
-                skipped.map(e => config.profiles.push(e));
-
-                fs.writeFile("./config.json", JSON.stringify(config, null, 4), (err) => {
-                    if (err) logger("err", "Error writing resolved profile ids to config: " + err);
-
-                    resolve(profiles);
+                    if (i == config.destinations.length - 1) resolve(results);
                 });
-            }
 
-        }, 500);
-
-    });
-};
-
-
-/**
- * Loads groups from config and converts them, if necessary, to steamID64
- * @returns {Promise.<string[]>} Returns a promise that resolves with an array of strings
- */
-module.exports.loadGroups = () => {
-    return new Promise((resolve) => {
-
-        let groups = [];
-        let resolverCalls = 0;
-        let skipped = [];
-        let emptyStr = 0;
-
-        config.groups.forEach((e) => {
-            if (String(e).includes("steamcommunity.com/groups")) {
-                resolverCalls++;
-
-                setTimeout(() => {
-                    steamidResolver.groupUrlToGroupID64(e, (err, groupID64) => {
-                        if (err) {
-                            logger("err", `I couldn't get the ID of ${e}. It will be excluded. Error: ${err}`);
-                            skipped.push(e);
-                        }
-
-                        groups.push(groupID64);
-                    });
-                }, 500 * resolverCalls);
-
-            } else {
-
-                if (e != "") groups.push(e);
-                    else emptyStr++;
-            }
+            }, 500 * i);
         });
-
-
-        let groupInterval = setInterval(() => {
-
-            if (groups.length + skipped.length + emptyStr == config.groups.length) {
-                clearInterval(groupInterval);
-
-                // Write result to make it
-                config.groups = groups;
-                skipped.map((e) => config.groups.push(e));
-
-                fs.writeFile("./config.json", JSON.stringify(config, null, 4), (err) => {
-                    if (err) logger("err", "Error writing resolved group ids to config: " + err);
-
-                    resolve(groups);
-                });
-            }
-
-        }, 500);
 
     });
 };
