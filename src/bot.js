c310a492ea88d385d85e0b9f6f0971bb4f10012b
3urobeat <tomke@goritz.org>
2025-06-25 19:26:34 +0200

    fix: Fix login error handler throwing error when logging #29

--- src/bot.js
+++ src/bot.js
@@ -4,10 +4,10 @@
  * Created Date: 2022-01-23 13:30:05
  * Author: 3urobeat
  *
- * Last Modified: 2024-11-02 15:08:38
+ * Last Modified: 2025-06-25 19:23:43
  * Modified By: 3urobeat
  *
- * Copyright (c) 2022 - 2024 3urobeat <https://github.com/3urobeat>
+ * Copyright (c) 2022 - 2025 3urobeat <https://github.com/3urobeat>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -268,6 +268,6 @@ module.exports.run = async () => {
             return;
         }
 
-        logger("error", `[${this.logOnOptions.accountName}] Error logging in! ${err}`);
+        logger("error", `Failed to log in! ${err}`);
     });
 };
